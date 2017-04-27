d24a52938e0a570b27acf80bb7e5ac97f0e96cee
haoshuaiwei <409328820@qq.com>
2017-04-27 15:14:02 +0800

    fix: swoole_server bug

--- Core/SwooleServer.php
+++ Core/SwooleServer.php
@@ -11,27 +11,20 @@ class SwooleServer
 {
     public $_serv = null;
     public $_host = '0.0.0.0';
-    public $_port = '9000';
+    public $_port = '9001';
 
 
     public function __construct()
     {
         $this->_serv = new \swoole_server($this->_host, $this->_port);
         $this->_serv->on('Connect', [$this, 'onConnect']);
-        $this->_serv->on('Request', [$this, 'onRequest']);
         $this->_serv->on('Receive', [$this, 'onReceive']);
-        $this->_serv->on('Close', [$this, 'onClose']);
         $this->_serv->start();
     }
 
     public function onConnect($serv, $fd)
     {
 
-    }
-
-    public function onRequest($request, $response)
-    {
-        die('不能进行持http请求');
     }
 
     public function onReceive($serv, $fd, $from_id, $data)
